388d6abb9f921387a497650c916dfd37bf12f296
vnalepa-rgb <v.nalepa@escola.pr.gov.br>
2026-09-02 11:22:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Mostra uma mensagem quando a página termina de carregar
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log(
+        "Site Além da Visão carregado com sucesso!"
+    );
+
+});
